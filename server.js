19d193c79a9457888e1bf036463426c20b5d4952
Nashid K <kp.nashid.nashid@gmail.com>
2024-10-27 02:45:40 +0530

    cors installed and applied

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+const cors = require('cors');
 
 // Load environment variables
 dotenv.config();
@@ -31,3 +32,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Enable CORS
+app.use(cors({ origin: 'http://localhost:3000' }));
